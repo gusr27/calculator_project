fa2ccb7111b329a5c6a406dad57807feab0aecb2
Gus Richardson <gus.richardson@icloud.com>
2017-03-28 17:04:28 -0400

    Added Calc Buttons and Functionality

--- scripts/script.js
+++ scripts/script.js
@@ -1,12 +1,46 @@
-$(document).ready(fucntion(){
-	
-})
+var html = "";
+var inputArr=[];
+
 
 function getInput(){
-	$(".numInput").on("click", function(c){
-		diplay($(".numInput").val());
-	});
+	var val = $(".numInput").val();
+	displayInput(val);
 }
 function displayInput(input){
-	$("#display").html(input);
+	inputArr.push(input);
+	//inputArr.map(function(c){ return parseInt(c);});
+	
+	$("#display").html("<p>"+inputArr.join("")+"</p>");
 }
+function clearInput(){
+	inputArr=[];
+	
+	$("#display").html("<p>"+inputArr.join("")+"</p>");
+}
+function submit(input){
+	
+	input = input.join("");
+	inputArr=[];
+	
+	var answer = math.eval(input);
+	
+	$("#display").html("<p>"+answer+"</p>");
+}
+$(document).ready(function(){
+	$("input:button").on("click", function(c){
+		c.preventDefault();
+		
+		displayInput($(this).val());
+	});
+	
+	$("#clear").on("click", function(c){
+		clearInput();
+	});
+	$("#enter").on("click", function(c){
+		c.preventDefault();
+		submit(inputArr);
+	});
+	
+
+	
+})
